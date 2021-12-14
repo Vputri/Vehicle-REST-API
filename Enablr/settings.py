fc2494e48ff0703564737a6accccb4e997958e57
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2021-12-14 20:53:12 +0700

    add

--- Enablr/settings.py
+++ Enablr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%hkmytgm=8g=@o&(7pq3qmi6nxu4bigg-h&me&w$l)-ufag4b^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vehicle-rest-api.herokuapp.com']
 
 
 # Application definition
