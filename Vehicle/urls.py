0fd1271199dd4f229e26327b3b719c0cba998e9a
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2021-12-14 21:36:20 +0700

    add

--- Vehicle/urls.py
+++ Vehicle/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import Price_ListView, home
 
 urlpatterns = [
-    path('pricelist/', Price_ListView.as_view()),
+    path('api/pricelist/', Price_ListView.as_view()),
     path('', home, name="home"),
 ]
